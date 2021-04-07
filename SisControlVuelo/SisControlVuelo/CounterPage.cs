c3f99895bd7b1a02e459f93b34a77de21da46d45
DiegoJavierJ <49220228+DiegoJavierJ@users.noreply.github.com>
2021-04-07 15:45:54 -0400

    CounterPage update fix

--- SisControlVuelo/SisControlVuelo/CounterPage.cs
+++ SisControlVuelo/SisControlVuelo/CounterPage.cs
@@ -14,5 +14,30 @@ namespace SisControlVuelo
         {
             InitializeComponent();
         }
+
+        private void CounterPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
