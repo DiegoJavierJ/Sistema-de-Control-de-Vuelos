c1344b779580ee1007168083e1508732d2662802
DiegoJavierJ <49220228+DiegoJavierJ@users.noreply.github.com>
2021-04-07 21:24:06 -0400

    Add btn's names

--- SisControlVuelo/SisControlVuelo/CounterPage.Designer.cs
+++ SisControlVuelo/SisControlVuelo/CounterPage.Designer.cs
@@ -29,49 +29,52 @@ namespace SisControlVuelo
         /// </summary>
         private void InitializeComponent()
         {
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.btn_RemoverVuelo = new System.Windows.Forms.Button();
+            this.btn_ModificarVuelo = new System.Windows.Forms.Button();
+            this.btn_AgregarAerolinea = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.panel12 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.btn_HistorialVuelo = new System.Windows.Forms.Button();
+            this.btn_AgregarVuelo = new System.Windows.Forms.Button();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.btn_CambiarCuenta = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel12.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button3
+            // btn_RemoverVuelo
             // 
-            this.button3.Location = new System.Drawing.Point(12, 207);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(189, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Remover Vuelo";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btn_RemoverVuelo.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_RemoverVuelo.Location = new System.Drawing.Point(12, 207);
+            this.btn_RemoverVuelo.Name = "btn_RemoverVuelo";
+            this.btn_RemoverVuelo.Size = new System.Drawing.Size(189, 23);
+            this.btn_RemoverVuelo.TabIndex = 2;
+            this.btn_RemoverVuelo.Text = "Remover Vuelo";
+            this.btn_RemoverVuelo.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btn_ModificarVuelo
             // 
-            this.button4.Location = new System.Drawing.Point(12, 145);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(189, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Modificar vuelo";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btn_ModificarVuelo.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_ModificarVuelo.Location = new System.Drawing.Point(12, 145);
+            this.btn_ModificarVuelo.Name = "btn_ModificarVuelo";
+            this.btn_ModificarVuelo.Size = new System.Drawing.Size(189, 23);
+            this.btn_ModificarVuelo.TabIndex = 3;
+            this.btn_ModificarVuelo.Text = "Modificar vuelo";
+            this.btn_ModificarVuelo.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btn_AgregarAerolinea
             // 
-            this.button5.Location = new System.Drawing.Point(12, 269);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(189, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Agregar Aerolinea";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.btn_AgregarAerolinea.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_AgregarAerolinea.Location = new System.Drawing.Point(12, 269);
+            this.btn_AgregarAerolinea.Name = "btn_AgregarAerolinea";
+            this.btn_AgregarAerolinea.Size = new System.Drawing.Size(189, 23);
+            this.btn_AgregarAerolinea.TabIndex = 4;
+            this.btn_AgregarAerolinea.Text = "Agregar Aerolinea";
+            this.btn_AgregarAerolinea.UseVisualStyleBackColor = true;
+            this.btn_AgregarAerolinea.Click += new System.EventHandler(this.button5_Click);
             // 
             // panel1
             // 
@@ -84,68 +87,15 @@ namespace SisControlVuelo
             this.panel1.Size = new System.Drawing.Size(814, 84);
             this.panel1.TabIndex = 6;
             // 
-            // button6
+            // label3
             // 
-            this.button6.Location = new System.Drawing.Point(399, 0);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(0, 0);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.AllowDrop = true;
-            this.button7.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button7.Location = new System.Drawing.Point(12, 21);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(189, 23);
-            this.button7.TabIndex = 9;
-            this.button7.Text = "Historial de Vuelo";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(12, 83);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(189, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Agregar Vuelo";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // panel12
-            // 
-            this.panel12.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.panel12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(76)))), ((int)(((byte)(153)))));
-            this.panel12.Controls.Add(this.button1);
-            this.panel12.Controls.Add(this.button5);
-            this.panel12.Controls.Add(this.button3);
-            this.panel12.Controls.Add(this.button4);
-            this.panel12.Controls.Add(this.button2);
-            this.panel12.Controls.Add(this.button7);
-            this.panel12.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel12.Location = new System.Drawing.Point(0, 84);
-            this.panel12.Name = "panel12";
-            this.panel12.Size = new System.Drawing.Size(214, 366);
-            this.panel12.TabIndex = 9;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(374, 191);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 15);
-            this.label1.TabIndex = 10;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 322);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(189, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Cambiar cuenta";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(163, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(544, 54);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Sistema de Control de Vuelos";
             // 
             // label2
             // 
@@ -157,15 +107,70 @@ namespace SisControlVuelo
             this.label2.TabIndex = 0;
             this.label2.Text = "SCV";
             // 
-            // label3
+            // button6
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(163, 19);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(640, 62);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Sistema de Control de Vuelos";
+            this.button6.Location = new System.Drawing.Point(399, 0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(0, 0);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // btn_HistorialVuelo
+            // 
+            this.btn_HistorialVuelo.AllowDrop = true;
+            this.btn_HistorialVuelo.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_HistorialVuelo.Location = new System.Drawing.Point(12, 21);
+            this.btn_HistorialVuelo.Name = "btn_HistorialVuelo";
+            this.btn_HistorialVuelo.Size = new System.Drawing.Size(189, 23);
+            this.btn_HistorialVuelo.TabIndex = 9;
+            this.btn_HistorialVuelo.Text = "Historial de Vuelo";
+            this.btn_HistorialVuelo.UseVisualStyleBackColor = true;
+            // 
+            // btn_AgregarVuelo
+            // 
+            this.btn_AgregarVuelo.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_AgregarVuelo.Location = new System.Drawing.Point(12, 83);
+            this.btn_AgregarVuelo.Name = "btn_AgregarVuelo";
+            this.btn_AgregarVuelo.Size = new System.Drawing.Size(189, 23);
+            this.btn_AgregarVuelo.TabIndex = 4;
+            this.btn_AgregarVuelo.Text = "Agregar Vuelo";
+            this.btn_AgregarVuelo.UseVisualStyleBackColor = true;
+            // 
+            // panel12
+            // 
+            this.panel12.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(76)))), ((int)(((byte)(153)))));
+            this.panel12.Controls.Add(this.btn_CambiarCuenta);
+            this.panel12.Controls.Add(this.btn_AgregarAerolinea);
+            this.panel12.Controls.Add(this.btn_RemoverVuelo);
+            this.panel12.Controls.Add(this.btn_ModificarVuelo);
+            this.panel12.Controls.Add(this.btn_AgregarVuelo);
+            this.panel12.Controls.Add(this.btn_HistorialVuelo);
+            this.panel12.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel12.Location = new System.Drawing.Point(0, 84);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(214, 366);
+            this.panel12.TabIndex = 9;
+            // 
+            // btn_CambiarCuenta
+            // 
+            this.btn_CambiarCuenta.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_CambiarCuenta.Location = new System.Drawing.Point(12, 322);
+            this.btn_CambiarCuenta.Name = "btn_CambiarCuenta";
+            this.btn_CambiarCuenta.Size = new System.Drawing.Size(189, 23);
+            this.btn_CambiarCuenta.TabIndex = 10;
+            this.btn_CambiarCuenta.Text = "Cambiar cuenta";
+            this.btn_CambiarCuenta.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(374, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // CounterPage
             // 
@@ -189,17 +194,17 @@ namespace SisControlVuelo
         }
 
         #endregion
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btn_RemoverVuelo;
+        private System.Windows.Forms.Button btn_ModificarVuelo;
+        private System.Windows.Forms.Button btn_AgregarAerolinea;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button btn_HistorialVuelo;
         private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_AgregarVuelo;
         private System.Windows.Forms.Panel panel12;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_CambiarCuenta;
     }
 }
