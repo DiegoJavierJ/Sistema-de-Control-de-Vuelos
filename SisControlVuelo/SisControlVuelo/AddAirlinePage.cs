b0e05377fb8ec7799a77b22c31f526e912f6bc0e
axellopez89 <axellpz89@hotmail.com>
2021-04-11 00:34:30 -0400

    AddAirline now saves to DB

--- SisControlVuelo/SisControlVuelo/AddAirlinePage.cs
+++ SisControlVuelo/SisControlVuelo/AddAirlinePage.cs
@@ -18,7 +18,19 @@ namespace SisControlVuelo
 
         private void btn_AgregarAerolinea_Click(object sender, EventArgs e)
         {
+            SqlConnection sql = ConnectionClass.Connect();
+            SqlCommand cmd = new SqlCommand("dbo.STP_Aerolinea_INS", sql);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@varNombre", textBox1.Text);
+            cmd.Parameters.AddWithValue("@varIDPais", int.Parse(textBox2.Text));
 
+            int i = cmd.ExecuteNonQuery();
+            sql.Close();
+
+            if(i != 0) 
+            {
+                MessageBox.Show(i + "Data Saved");
+            }
         }
     }
 }
